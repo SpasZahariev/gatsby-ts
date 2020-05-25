05e187e4a057e6a46041acefd7d4104e902318c3
Spas_Z <spaszahariev54@gmail.com>
2020-05-25 19:50:58 +0100

    added pictures

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
 
     <div>
+      {navigationMenu()}
       {getIntroduction()}
       {getBackgroundParagraph()}
       {getExperienceParagraph()}
@@ -15,43 +16,56 @@ const App = () => {
   );
 }
 
+const navigationMenu = () => {
+  return <div></div>
+}
 
 
-function getIntroduction() {
+const getIntroduction = () => {
   return <section>
-    <h1>Hello, there - I'm SPAS</h1>
-    <h4>Reach out button... pdf resume button</h4>
+    <h4>Hello, there - I'm</h4>
+    <h1>SPAS ZAHARIEV</h1>
+    <h1>I like creating things and solving problems</h1>
+    <h4>I'm a backend software engineer based in the UK, focusing on scalable and fault tolerant solutions</h4>
+    <button>Reach out pdf</button>
+    <button>Resume</button>
   </section>
 }
 
-function getBackgroundParagraph() {
+const getBackgroundParagraph = () => {
   return <section>
-    <h1>my life story</h1>
+    <h1>About Me</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore unde consequuntur ex voluptates ad? Dolore, sapiente ea cum consequuntur eaque adipisci tempora nostrum quaerat ad sequi! Nostrum consectetur maiores libero?
     </p>
   </section>
 }
 
-function getExperienceParagraph() {
+const getExperienceParagraph = () => {
   return <section>
     <h1>experience timeline</h1>
   </section>
 }
 
-function getSkillsTable() {
+const getSkillsTable = () => {
   return <section>
+    <h1>Skills</h1>
     <h1>Look at some skills</h1>
   </section>
 }
 
-function getProjects() {
+const getProjects = () => {
   return <section>
     <h1>Featured Projects</h1>
+    <div>
+      <img src="https://picsum.photos/500/800"></img>
+      <img src="https://picsum.photos/500/800"></img>
+      <img src="https://picsum.photos/500/800"></img>
+    </div>
   </section>
 }
 
-function getContactInfo() {
+const getContactInfo = () => {
   return <section>
     <h1>Contact Info</h1>
   </section>
